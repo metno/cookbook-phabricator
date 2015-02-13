09b9bf81d5b302addd7d535174f842f079d50dbb
Kim Tore Jensen <kimtj@met.no>
2015-02-13 16:08:40 +0100

    Release version 2.0.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    'kimtj@met.no'
 license             'GNU GPL 2'
 description         'Installs and configures Phabricator'
 long_description    ''
-version             '1.4.1'
+version             '2.0.0'
 
 supports            'ubuntu', '= 12.04'
 supports            'ubuntu', '= 14.04'
