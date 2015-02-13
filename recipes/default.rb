metno/cookbook-phabricator

commit 77f3314daee92604c8fe29071537f5e0ffeadf61

Multiple fixes and upgrades
Summary:
The 1.4.1 version does not always work, partly because of issues with external cookbooks. This patch addresses the issue.

Note that this version is NOT backwards compatible, so a major version bump is needed.

Tasks: fixes T442

* Upgrade cookbook dependencies
* Fix integration tests
* Make sure the cookbook converges
* Replace init script with Upstart job

Test Plan: `kitchen test default`

Reviewers: #phabricator, michaeloa

Reviewed By: #phabricator, michaeloa

Subscribers: michaeloa

Projects: #phabricator

Maniphest Tasks: T442

Differential Revision: https://phab.met.no/D240

recipes/default.rb
@@ -8,8 +8,8 @@
 #
 
 # Copy some parameters into explicit Phabricator configuration
-node.set['phabricator']['config']['repository.default-local-path'] = node['phabricator']['repository_path']
-node.set['phabricator']['config']['phd.user'] = node['phabricator']['user']
+node.default['phabricator']['config']['repository.default-local-path'] = node['phabricator']['repository_path']
+node.default['phabricator']['config']['phd.user'] = node['phabricator']['user']
 
 # Some default Phabricator settings
 node.default['phabricator']['config']['metamta.domain'] = node['phabricator']['domain']
@@ -32,15 +32,31 @@ end
 nginx_available_conf = "/etc/nginx/sites-available/#{node['phabricator']['domain']}.conf" 
 nginx_enabled_conf = "/etc/nginx/sites-enabled/#{node['phabricator']['domain']}.conf" 
 
-mysql_connection = {
-    :host => 'localhost',
-    :user => 'root',
-    :password => node['mysql']['server_root_password']
-}
-
 # Make sure the package list is up to date
 include_recipe 'apt'
 
+# Make sure MySQL has a default root password
+if not node.run_state.has_key?('mysql_root_password') then
+    Chef::Log.warn("****** Your MySQL password is set to an insecure default value, please modify it using `node.run_state['mysql_root_password']`! ******")
+    node.run_state['mysql_root_password'] = 'pleaserootmydatabase'
+end
+
+# Set up a MySQL server
+mysql_service 'default' do
+    initial_root_password node.run_state['mysql_root_password']
+    version node['mysql']['version']
+    action [:create, :start]
+end
+
+# Define MySQL connection credentials
+mysql_connection = {
+    :host => '127.0.0.1',
+    :port => 3306,
+    :user => 'root',
+    :password => node.run_state['mysql_root_password']
+}
+
+# Install required packages for Phabricator
 node['phabricator']['packages'].each do |p|
     package p do
         action :upgrade
@@ -64,7 +80,6 @@ end
 include_recipe "php"
 include_recipe "php-fpm"
 include_recipe "nginx"
-include_recipe "mysql::server"
 include_recipe "database::mysql"
 
 # Unfortunately, the PHP-FPM recipe does not have any concept of how to
@@ -84,7 +99,7 @@ template "/etc/mysql/conf.d/phabricator_sql_mode.cnf" do
     mode "0644"
     owner "root"
     group "root"
-    notifies :restart, "service[mysql]"
+    notifies :restart, "mysql_service[default]"
 end
 
 group node['phabricator']['group'] do
@@ -135,15 +150,11 @@ php_fpm_pool "phabricator" do
     php_options 'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => node['phabricator']['php_memory_limit']
 end
 
-template "/etc/init.d/phd" do
-    source "phd.erb"
-    user "root"
-    group "root"
-    mode "0755"
-    variables :vars => {
-        :node => node
-    }
-    notifies :restart, "service[phd]"
+template '/etc/init/phd.conf' do
+    source 'upstart/phd.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
 end
 
 template "/etc/logrotate.d/phd" do
@@ -201,12 +212,15 @@ execute "upgrade_phabricator_databases" do
     notifies :stop, "service[php-fpm]", :immediately
     notifies :stop, "service[phd]", :immediately
     notifies :run, "execute[run_storage_upgrade]", :immediately
-    notifies :start, "service[phd]", :immediately
+    # phd daemon omitted because it is defined as a service
+    # that will start anyway
     notifies :start, "service[php-fpm]", :immediately
 end
 
 execute "run_storage_upgrade" do
     command "#{node['phabricator']['path']}/phabricator/bin/storage upgrade --force"
+    user node['phabricator']['user']
+    group node['phabricator']['group']
     action :nothing
 end
 
@@ -218,12 +232,14 @@ node['phabricator']['config'].each do |key, value|
     end
 end
 
-service "phd" do
-    action :enable
-end
-
-service "mysql" do
+service 'phd' do
+    supports :status => true, :restart => true, :start => true, :stop => true
+    provider Chef::Provider::Service::Upstart
     action [:enable, :start]
 end
 
-node.set['phabricator']['storage_upgrade_done'] = true
+ruby_block 'set_storage_upgrade_done' do
+    block do
+        node.set['phabricator']['storage_upgrade_done'] = true
+    end
+end

attributes/default.rb
@@ -31,6 +31,9 @@ default['phabricator']['mysql_port'] = '3306'
 default['phabricator']['mysql_user'] = 'phabricator'
 default['phabricator']['mysql_password'] = 'changeme'
 
+# MySQL version
+default['mysql']['version'] = '5.5'
+
 # PHP memory limit
 default['phabricator']['php_memory_limit'] = '128M'
 
@@ -39,7 +42,7 @@ default['phabricator']['storage_upgrade_done'] = false
 
 # MySQL option innodb_buffer_pool_size
 mem = node['memory']['total'].split("kB")[0].to_i*1024*0.4
-node.default['phabricator']['innodb_buffer_pool_size'] = mem.to_int
+default['phabricator']['innodb_buffer_pool_size'] = mem.to_int
 
 # Various config settings, feel free to expand with your own stuff.
 default['phabricator']['config'] = {
@@ -48,7 +51,7 @@ default['phabricator']['config'] = {
 
 # Package dependencies
 default['phabricator']['packages'] = [
-    'git', 'php5', 'php5-mysql', 'php5-gd', 'php5-dev', 'php5-curl', 'php-apc', 'php5-cli', 'php5-json'
+    'curl', 'git', 'php5', 'php5-mysql', 'php5-gd', 'php5-dev', 'php5-curl', 'php-apc', 'php5-cli', 'php5-json'
 ]
 
 # Where to put Arcanist when using the arcanist recipe

.kitchen.yml
@@ -7,13 +7,16 @@ provisioner:
 
 platforms:
   - name: ubuntu-12.04
-  - name: debian-7.3
 
 suites:
   - name: default
     run_list:
       - recipe[phabricator::default]
-    attributes:
+    attributes: {
+        "phabricator": {
+            "domain": "phabricator.example.com"
+        }
+    }
   - name: arcanist
     run_list:
       - recipe[phabricator::arcanist]

metadata.rb
@@ -10,8 +10,8 @@ supports            'debian', '~> 7.0'
 supports            'ubuntu', '= 12.04'
 
 depends             'apt',      '~> 2.6'
-depends             'php',      '~> 1.4'
-depends             'php-fpm',  '~> 0.6'
+depends             'php',      '~> 1.5'
+depends             'php-fpm',  '~> 0.7'
 depends             'nginx',    '~> 2.7'
-depends             'mysql',    '~> 5.5'
-depends             'database', '~> 2.3'
+depends             'mysql',    '~> 6.0'
+depends             'database', '~> 3.1'

templates/default/phd-logrotate.erb
@@ -8,9 +8,9 @@
     create 640 <%= node['phabricator']['user'] %> <%= node['phabricator']['group'] %>
     sharedscripts
     prerotate
-        /etc/init.d/phd stop >/dev/null
+        service phd stop >/dev/null
     endscript
     postrotate
-        /etc/init.d/phd start >/dev/null
+        service phd start >/dev/null
     endscript
 }

templates/default/phd.erb
@@ -1,67 +1,0 @@
-#!/bin/sh
-# Stolen from https://github.com/rbjavier/puppet-phabricator/blob/master/templates/phd.erb
-# This file is served by Chef. Your contribution will perish. Resistance is futile!
-#
-### BEGIN INIT INFO
-# Provides: phd
-# Required-Start: $remote_fs $syslog
-# Required-Stop: $remote_fs $syslog
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Wrapper around phabricator's phd daemon launcher
-### END INIT INFO
-
-DESC="phd daemon launcher"
-NAME=phd
-DAEMON=<%= @node['phabricator']['path'] %>/phabricator/bin/$NAME
-SCRIPTNAME=/etc/init.d/$NAME
-
-# Exit if the package is not installed
-[ -x "$DAEMON" ] || exit 1
-
-# Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
-
-# Define LSB log_* functions.
-. /lib/lsb/init-functions
-
-RETVAL=0
-case "$1" in
-    start)
-        [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
-        "$DAEMON" start
-        RETVAL="$?"
-        case "$?" in
-            0) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-            1) [ "$VERBOSE" != no ] && log_end_msg 255 ;; # Already started
-            *) [ "$VERBOSE" != no ] && log_end_msg 1 ;; # Failed to start
-        esac
-        ;;
-    stop)
-        [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
-        "$DAEMON" stop
-        RETVAL="$?"
-        case "$?" in
-            0) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-            *) [ "$VERBOSE" != no ] && log_end_msg 1 ;; # Failed to stop
-        esac
-        ;;
-    status)
-        "$DAEMON" status && exit 0 || exit $?
-        ;;
-    restart)
-        log_daemon_msg "Restarting $DESC" "$NAME"
-        "$DAEMON" restart
-        RETVAL="$?"
-        case "$?" in
-            0) log_end_msg 0 ;;
-            *) log_end_msg 1 ;; # Failed to restart
-        esac
-        ;;
-    *)
-        echo "Usage: $SCRIPTNAME {start|stop|status|restart}" >&2
-        exit 3
-        ;;
-esac
-
-exit "$RETVAL"

templates/default/upstart/phd.erb
@@ -1,0 +1,32 @@
+#
+# This file is served by Chef. Your contribution will perish. Resistance is futile!
+#
+
+description     "Phabricator daemon"
+author          "chef-client"
+
+start on started mysql-default
+stop on stopping mysql-default
+
+setuid <%= node['phabricator']['user'] %>
+setgid <%= node['phabricator']['group'] %>
+
+respawn
+
+#
+# Phabricator has its own daemon process manager. This script, in addition to
+# the pre-start and post-stop scripts, are hacks that circumvent this.
+#
+script
+    while [ "$(ls -A /var/tmp/phd/pid)" ]; do
+        sleep 1
+    done
+end script
+
+pre-start script
+    <%= node['phabricator']['path'] %>/phabricator/bin/phd start
+end script
+
+post-stop script
+    <%= node['phabricator']['path'] %>/phabricator/bin/phd stop
+end script

test/integration/default/bats/default.bats
@@ -11,8 +11,13 @@
     [ $status -eq 0 ]
 }
 
+@test "Phabricator daemons are running" {
+    run service phd status
+    [ $status -eq 0 ]
+}
+
 @test "Phabricator responds to HTTP requests" {
-    wget -O - http://localhost | {
+    curl http://localhost | {
         run grep Phabricator
         [ $status -eq 0 ]
     }

commit 2b64dfc1cbbf3030b787cf099ca35d2a7164b37f

Fix broken MySQL configuration
Summary:
When mysql cookbook version was upgraded to 6.0, custom MySQL configuration broke. This patch fixes this.

Bug introduced in 77f3314daee92604c8fe29071537f5e0ffeadf61.

Test Plan: N/A

Reviewers: #phabricator, espenm

Reviewed By: espenm

Projects: #phabricator

Differential Revision: https://phab.met.no/D250

recipes/default.rb
@@ -81,12 +81,11 @@ link "/etc/php5/fpm/php.ini" do
 end
 
 # Phabricator needs special MySQL configuration.
-template "/etc/mysql/conf.d/phabricator_sql_mode.cnf" do
-    source "phabricator.cnf.erb"
-    mode "0644"
-    owner "root"
-    group "root"
-    notifies :restart, "mysql_service[default]"
+mysql_config 'phabricator' do
+    source 'phabricator.cnf.erb'
+    instance 'default'
+    action :create
+    notifies :restart, 'mysql_service[default]'
 end
 
 group node['phabricator']['group'] do