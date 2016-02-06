a8a52f294f0dd5cbf4af8a6a656e1e3f1cc1ab7b
Martin Grønlien Pejcoch <mgp@met.no>
2016-02-06 15:18:07 +0100

    Add a pacakge that is necessary for enabling LDAP support in Phabricator
    Test Plan: kitchen verify
    
    Reviewers: #phabricator, michaeloa
    
    Reviewed By: #phabricator, michaeloa
    
    Maniphest Tasks: T140
    
    Differential Revision: https://phab.met.no/D1528

--- attributes/default.rb
+++ attributes/default.rb
@@ -68,7 +68,7 @@ default['phabricator']['config'] = {
 
 # Package dependencies
 default['phabricator']['packages'] = [
-    'curl', 'git', 'php5', 'php5-mysql', 'php5-gd', 'php5-dev', 'php5-curl',
+    'curl', 'git', 'php5', 'php5-ldap', 'php5-mysql', 'php5-gd', 'php5-dev', 'php5-curl',
     'php-apc', 'php5-cli', 'php5-json', 'python-pygments',
 ]
 
